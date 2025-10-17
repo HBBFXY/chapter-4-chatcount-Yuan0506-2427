a71ce0b950e084cdb2dcf0e6c80572b4f288756c
Yuan0506-2427 <3059560018@qq.com>
2025-10-17 11:38:39 +0800

    Remove unnecessary blank lines in main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import string
-
 s = input("请输入一行字符:")
 letter_count = 0
 digit_count = 0
 space_count = 0
 other_count = 0
-
 for char in s:
     if char in string.ascii_letters:
         letter_count += 1
@@ -15,7 +13,6 @@ for char in s:
         space_count += 1
     else:
         other_count += 1
-
 print("英文字符:", letter_count)
 print("数字:", digit_count)
 print("空格:", space_count)
