667af88020c31f80071d394e7373d112f873be42
Yuan0506-2427 <3059560018@qq.com>
2025-10-17 10:48:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# 这个文件用于编写代码
+s=input("请输入一行字符:")
+letter count=0
+digit count=0
+space count=0
+other count=0
+for char in s:
+  if char.isalpha():
+    letter count+=1
+  elif char.isdigit():
+    digit count+=1
+  elif char.isspace():
+    space count+=1
+  else:
+    other count+=1
+print("英文字符:",letter count)
+print("数字:",digit count)
+print("空格:",space count)
+print("其他字符:",other count)
