9c19a7039df070dbb6892ee3c37ac1a31c3c8af0
Yuan0506-2427 <3059560018@qq.com>
2025-10-17 11:41:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import string
+
 s = input("请输入一行字符:")
 letter_count = 0
 digit_count = 0
 space_count = 0
 other_count = 0
+
 for char in s:
     if char in string.ascii_letters:
         letter_count += 1
-    elif char.isdigit():
+    elif char in string.digits:
         digit_count += 1
     elif char.isspace():
         space_count += 1
     else:
         other_count += 1
+
 print("英文字符:", letter_count)
 print("数字:", digit_count)
 print("空格:", space_count)
-print("其他字符:", other_count)
+print("其他:", other_count)
