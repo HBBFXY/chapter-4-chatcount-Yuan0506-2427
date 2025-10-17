f8f6ddb92942e46e5231d686e36dae8edbebae4f
Yuan0506-2427 <3059560018@qq.com>
2025-10-17 10:56:57 +0800

    Fix variable naming and improve character counting

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-s=input("请输入一行字符:")
-letter count=0
-digit count=0
-space count=0
-other count=0
+s = input("请输入一行字符:")
+letter_count = 0
+digit_count = 0
+space_count = 0
+other_count = 0
+
 for char in s:
-  if char.isalpha():
-    letter count+=1
-  elif char.isdigit():
-    digit count+=1
-  elif char.isspace():
-    space count+=1
-  else:
-    other count+=1
-print("英文字符:",letter count)
-print("数字:",digit count)
-print("空格:",space count)
-print("其他字符:",other count)
+    # 只统计 ASCII 英文字母作为“英文字符”
+    if char.isalpha() and char.isascii():
+        letter_count += 1
+    elif char.isdigit():
+        digit_count += 1
+    elif char.isspace():
+        space_count += 1
+    else:
+        other_count += 1
+
+print("英文字符:", letter_count)
+print("数字:", digit_count)
+print("空格:", space_count)
+print("其他字符:", other_count)
